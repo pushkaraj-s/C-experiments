5f59d3d2ef76350e36ae0d7f10860fbb39828f46
pushkaraj-s <said.pushkar@gmail.com>
2026-03-26 17:43:04 -0500

    Updated Q8

--- Q8_Print_True_or_False.C
+++ Q8_Print_True_or_False.C
@@ -2,5 +2,14 @@
 
 int main(){
 
-    
+	//Print 1(true) or 0(false) for following statements:
+
+	// a. if it's sunday and it's snowing -> true
+	// b. if it's monday and it's raining -> true
+	// c. if number is greater than 9 and less than 100 -> true
+
+	int isSunday = 1;
+	int isSnowing = 1;
+
+   	printf("%d \n", isSnowing && isSunday);
 }
