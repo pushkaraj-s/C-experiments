d2b5698e1abe7ce981c28b851b1e291e63743ad6
pushkaraj-s <said.pushkar@gmail.com>
2026-04-01 15:12:28 -0500

    Sloved Q15 and Q16

---
+++ Q15_Sum_of_First_n_Natural_Numbers.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+	int n=0;
+	int sum=0;
+	printf("Enter a number: ");
+	scanf("%d" , &n);
+
+
+	for(int i=0; i <= n; i++){
+		sum = sum + i;
+	}
+	printf("Sum of first %d numbers is: %d\n \n", n, sum);
+
+
+	printf("Numbers in reverse order: \n");
+	
+	// Debug Print Statement
+	// printf("%d", n);
+	for(int j=n; j >= 0; j--){
+		printf("%d \n", j);
+	}
+
+	return 0;
+}
