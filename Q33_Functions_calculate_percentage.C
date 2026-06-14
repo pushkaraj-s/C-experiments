c15ddc570bf01e87e4eccd92e6a18300d66e6360
pushkaraj-s <said.pushkar@gmail.com>
2026-06-14 16:58:56 -0700

    Sloved Q33

--- Q33_Functions_calculate_percentage.C
+++ Q33_Functions_calculate_percentage.C
@@ -1,2 +1,17 @@
 #include <stdio.h>
 
+int calcPercentage(int science, int math, int sanskrit);
+
+int main() {
+    int science = 85;
+    int math = 90;
+    int sanskrit = 75;
+    int percentage = calcPercentage(science, math, sanskrit);
+    printf("Percentage: %d\n", percentage);
+    return 0;
+}
+
+int calcPercentage(int science, int math, int sanskrit) {
+    int total = science + math + sanskrit;
+    return (total * 100) / 300;
+}
