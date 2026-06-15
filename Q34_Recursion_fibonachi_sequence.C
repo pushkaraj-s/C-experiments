e0b2ca70c7f642047f7f156a0edde854b2932a24
pushkaraj-s <said.pushkar@gmail.com>
2026-06-14 17:19:21 -0700

    Solved Q34

---
+++ Q34_Recursion_fibonachi_sequence.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fib(int n);
+
+int main() {
+  	//Write a function to print n terms of the fibonachi sequence using recursion
+    // fib(n) = fib(n-1) + fib(n-2)
+    printf("This is the result = %d \n", fib(6));    
+       
+    return 0;
+}
+
+int fib(int n){
+    if(n==0){
+        return 0;
+    }      
+    if (n==1){
+        return 1;
+    }
+     
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    //printf("Fib of %d is: %d \n", n, fibN);  
+    return fibN;
+}
