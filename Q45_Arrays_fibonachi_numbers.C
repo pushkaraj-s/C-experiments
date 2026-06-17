13814dc46ee646c72d329778ca9e57b001818dbf
pushkaraj-s <said.pushkar@gmail.com>
2026-06-17 00:44:00 -0700

    Solved Q45 and Q46

--- Q45_Arrays_fibonachi_numbers.C
+++ Q45_Arrays_fibonachi_numbers.C
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
 //Write a program to store the first n Fibonacci numbers in an array and print them.
+// These are fibonacci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+// nth number = (n-1)th number + (n-2)th number
+
+int main() {
+    int n;
+    printf("Enter n(n>2); ");
+    scanf("%d", &n);
+
+    int fib[n];
+    fib[0] = 0;
+    fib[1] = 1; //These 2 are always constant
+
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]; // Calculate the next Fibonacci number
+    }
+
+    printf("The first %d Fibonacci numbers are: ", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fib[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ Q46_Arrays_table_of_2_and_3.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void storeTable(int arr[][10], int n, int m, int number);
+
+int main(){
+    
+    //Create a 2D array, storing the table of 2 and 3.
+    
+    int tables[2][10];
+    storeTable(tables, 0, 10, 2);
+    storeTable(tables, 1, 10, 3);
+    
+    for(int i=0; i<10; i++){ //0 to 10
+        printf("%d \t", tables[0][i]);
+    }  
+    
+    printf("\n");
+    
+    for(int i=0; i<10; i++){ //0 to 10
+        printf("%d \t", tables[1][i]);
+    }    
+
+    printf("\n");
+    
+    return 0;
+
+}
+
+void storeTable(int arr[][10], int n, int m, int number){
+    for(int i=0; i<m; i++){
+        arr[n][i] = number *(i+1);
+    }
+}
