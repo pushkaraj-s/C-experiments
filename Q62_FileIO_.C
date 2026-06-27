6a2e566bf9b05cb31511838175eb6b3d61a6f7f2
pushkaraj-s <said.pushkar@gmail.com>
2026-06-26 20:29:09 -0700

    Create Q62_FileIO_.C
    Initial setup for student information input program.

---
+++ Q62_FileIO_.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    //Write a program to input student information from a user & enter it to a file
+    
+    char name[50];
+    int age;
+    float cgpa;
+    
+    
+    return 0;
+}
