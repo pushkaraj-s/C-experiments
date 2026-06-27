3fe0f00eb653e57b10fc2b785abc3eec121de102
pushkaraj-s <said.pushkar@gmail.com>
2026-06-26 23:43:28 -0700

    Q62 Solved

--- Q62_FileIO_.C
+++ Q62_FileIO_.C
@@ -2,11 +2,24 @@
 
 int main(){
     //Write a program to input student information from a user & enter it to a file
+    FILE *fptr;
+    fptr = fopen("student.txt", "w");
     
     char name[50];
     int age;
     float cgpa;
     
+    printf("Enter Name: ");
+    scanf("%s", name);
+    printf("Enter Age: ");
+    scanf("%d", &age);
+    printf("Enter CGPA: ");
+    scanf("%f", &cgpa);
     
+    fprintf(fptr, "Student Name: %s\n", name);
+    fprintf(fptr, "Student Age: %d\t", age);
+    fprintf(fptr, "Student CGPA: %f\t", cgpa);
+    
+    fclose(fptr);
     return 0;
 }
